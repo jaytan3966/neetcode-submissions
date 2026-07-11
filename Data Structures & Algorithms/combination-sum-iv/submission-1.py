8e41d977825e40923c3ca34138f34b89698c69c4
Jayden Tan <jaytan3966@gmail.com>
2026-07-10 22:15:46 -0700

    Add: combination-sum-iv - submission-1

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        
+        memo = {}
+
+        def dfs(total):
+
+            if total == target:
+                return 1
+            
+            if total > target:
+                return 0
+            
+            if total in memo: return memo[total]
+            
+            ans = 0
+            for num in nums:
+                ans += dfs(total+num)
+
+            memo[total] = ans
+            return ans
+
+        return dfs(0)
