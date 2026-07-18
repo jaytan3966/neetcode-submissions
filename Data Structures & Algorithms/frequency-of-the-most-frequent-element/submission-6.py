8ceda98862c74cf5c8cea5fca0587ae635905770
Jayden Tan <jaytan3966@gmail.com>
2026-07-17 20:16:15 -0700

    Add: frequency-of-the-most-frequent-element - submission-6

---
+++ Data Structures & Algorithms/frequency-of-the-most-frequent-element/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+
+        nums.sort()
+        n = len(nums)
+        ans = 1
+
+        for i in range(n):
+            if i<n-1 and nums[i] == nums[i+1]: continue
+            rem = k
+            p = i+1
+            same = 1
+            while rem>0:
+                if p<n and (nums[p]-nums[i])<=rem:
+                    rem-=((nums[p]-nums[i]))
+                    p+=1
+                    same+=1
+                    print(i, p)
+                else: break
+            ans = max(ans, same)
+        return ans
+
+        
