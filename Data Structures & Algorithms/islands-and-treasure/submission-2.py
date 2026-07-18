d04243df621ddac5e7118485e2261fc6f80d5f45
Jayden Tan <jaytan3966@gmail.com>
2026-07-18 16:51:53 -0700

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        q = deque([])
+        seen = set()
+
+        n, m = len(grid), len(grid[0])
+        for r in range(n):
+            for c in range(m):
+                if grid[r][c] == 0:
+                    q.append((r,c))
+                    seen.add((r,c))
+
+        dirs = [(1,0), (-1,0), (0,1), (0,-1)]
+        dist = 0
+        while q:
+            for _ in range(len(q)):
+                i, j = q.popleft()
+                seen.add((i,j))
+                grid[i][j] = min(grid[i][j], dist)
+                for y, x in dirs:
+                    if 0<=i+y<n and 0<=j+x<m and grid[i+y][j+x] != -1 and (i+y,j+x) not in seen:
+                        q.append((i+y,j+x))
+            dist+=1
+
+
