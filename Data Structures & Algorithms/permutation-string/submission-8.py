2f2fc4a35ec3994aede4a99c2f12d45937e98c1a
Jayden Tan <jaytan3966@gmail.com>
2026-07-19 17:35:08 -0700

    Add: permutation-string - submission-8

---
+++ Data Structures & Algorithms/permutation-string/submission-8.py
@@ -0,0 +1,22 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        
+        counts = Counter(s1)
+        og = counts
+        l, r = 0, 0
+
+        while r<len(s2):
+            if s2[r] in counts and counts[s2[r]] != 0: 
+                counts[s2[r]]-=1
+                if all(x == 0 for x in counts.values()): return True
+                r+=1
+            else:
+                # if s2[l] in counts: counts[s2[l]]+=1
+                counts = og
+                l+=1
+                r=l
+            print(counts) 
+            
+        return False
+        
+
