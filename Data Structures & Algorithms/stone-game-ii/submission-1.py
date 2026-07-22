229d22b742ceab4fcb562bcc48d2e5ecc91542f4
Jayden Tan <jaytan3966@gmail.com>
2026-07-22 10:15:42 -0700

    Add: stone-game-ii - submission-1

---
+++ Data Structures & Algorithms/stone-game-ii/submission-1.py
@@ -0,0 +1,26 @@
+from functools import lru_cache
+from typing import List
+
+class Solution:
+    def stoneGameII(self, piles: List[int]) -> int:
+        n = len(piles)
+
+        suffix = [0] * (n + 1)
+        for i in range(n - 1, -1, -1):
+            suffix[i] = suffix[i + 1] + piles[i]
+
+        @lru_cache(None)
+        def dp(i: int, M: int) -> int:
+            if i + 2 * M >= n:
+                return suffix[i]
+
+            best = 0
+
+            for X in range(1, 2 * M + 1):
+                opponent = dp(i + X, max(M, X))
+                current = suffix[i] - opponent
+                best = max(best, current)
+
+            return best
+
+        return dp(0, 1)
