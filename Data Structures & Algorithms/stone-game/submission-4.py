bda233e49bd531cb7d92677498e2ca18e8b1e96f
Jayden Tan <jaytan3966@gmail.com>
2026-08-04 16:46:11 -0700

    Add: stone-game - submission-4

---
+++ Data Structures & Algorithms/stone-game/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        n = len(piles)
+        dp = [[-1 for _ in range(n)] for _ in range(n)]
+
+        def dfs(l,r):
+            if l>r: return 0
+            if dp[l][r] != -1: return dp[l][r]
+
+            turn = (r-l)%2 == 0
+            left = piles[l] if turn else 0
+            right = piles[r] if turn else 0
+            dp[l][r] = max(dfs(l+1,r)+left, dfs(l,r-1)+right)
+            return dp[l][r]
+        
+        total = sum(piles)
+        return dfs(0,n-1)>(total//2)
+
+
