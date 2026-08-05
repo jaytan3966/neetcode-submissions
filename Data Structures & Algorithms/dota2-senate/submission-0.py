1b458a57d6081e6ddad0ce1f8f2424c78c4d10ed
Jayden Tan <jaytan3966@gmail.com>
2026-08-05 10:18:59 -0700

    Add: dota2-senate - submission-0

---
+++ Data Structures & Algorithms/dota2-senate/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        inn = deque([])
+        out = deque([])
+
+        n = len(senate)
+
+        return "Radiant"
+        
+        # for i in range(n):
+        #     if inn
