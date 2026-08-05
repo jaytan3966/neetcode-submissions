3a1217b0f1439efe34ffd6acd632d0b461045074
Jayden Tan <jaytan3966@gmail.com>
2026-08-04 19:31:14 -0700

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.py
@@ -0,0 +1,20 @@
+class TimeMap:
+
+    def __init__(self):
+        self.pairs = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.pairs[key].append((value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        arr = self.pairs[key]
+        l, r = 0, len(arr)-1
+
+        while l<=r:
+            mid = (l+r)//2
+            if arr[mid][1] == timestamp: return arr[mid][0]
+            elif arr[mid][1] > timestamp: r = mid-1
+            else: l = mid+1
+        print(arr)
+        if r>=0: return arr[r][0]
+        return ""
