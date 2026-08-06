acb95cb6a122728ddbdfa33a53650664bcc20ea6
Jayden Tan <jaytan3966@gmail.com>
2026-08-05 19:43:38 -0700

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s)%2 != 0:
+            return False
+        
+        pairs = {')':'(', "}": "{", "]": "["}
+        stack = []
+
+        for char in s:
+            if char not in pairs:
+                stack.append(char)
+            else:
+                if not stack or pairs[char] != stack.pop():
+                    return False
+        return len(stack)==0
