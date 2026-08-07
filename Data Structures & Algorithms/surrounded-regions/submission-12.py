02a395678be2635b90a30acff488b19f950225e7
Jayden Tan <jaytan3966@gmail.com>
2026-08-07 15:39:35 -0700

    Add: surrounded-regions - submission-12

---
+++ Data Structures & Algorithms/surrounded-regions/submission-12.py
@@ -0,0 +1,26 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        n = len(board)
+        m = len(board[0])
+        dirs = [(0,1), (0,-1), (1,0), (-1,0)]
+        invalid = set()
+
+        def dfs(r,c):
+            if not (0<=r<n and 0<=c<m): return
+            if board[r][c] == 'X': return
+
+            invalid.add((r,c))
+            for y,x in dirs:
+                if (r+y,c+x) not in invalid:
+                    dfs(r+y,c+x)
+
+        for r in range(n):
+            dfs(r,0)
+            dfs(r,m-1)
+        for c in range(m):
+            dfs(0,c)
+            dfs(n-1,c)
+        print(invalid)
+        for r in range(n):
+            for c in range(m):
+                if board[r][c] == 'O' and (r,c) not in invalid: board[r][c] = 'X'
