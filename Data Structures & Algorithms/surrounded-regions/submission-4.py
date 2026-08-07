9d84da0b1eb48352e7f7f8738c0400beba90a2c5
Jayden Tan <jaytan3966@gmail.com>
2026-08-06 21:10:25 -0700

    Add: surrounded-regions - submission-4

---
+++ Data Structures & Algorithms/surrounded-regions/submission-4.py
@@ -0,0 +1,32 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        n = len(board)
+        m = len(board[0])
+        dirs = [(1,0), (-1,0), (0,1), (0,-1)]
+        seen = set()
+
+        def dfs(r,c):
+            if not 0<=r<n or not 0<=c<m:
+                return
+            if board[r][c] == 'X' or board[r][c] == 'W':
+                return 
+            
+            found = False
+            for y,x in dirs:
+                if 0<=r+y<n and 0<=c+x<m:
+                    if board[r+y][c+x] == 'O' or board[r+y][c+x] == 'W':
+                        seen.add((r,c))
+                        found = True
+                        break
+            if not found:
+                board[r][c] == 'X'
+            
+        for r in range(n):
+            for c in range(m):
+                if board[r][c] == 'O':
+                    dfs(r,c)
+        for r,c in seen:
+            board[r][c] = 'X'
+                    
+
+        
