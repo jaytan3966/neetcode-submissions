ccf669fd383b6bb1491ffed27e7688cde0e06ddd
Jayden Tan <jaytan3966@gmail.com>
2026-08-06 19:35:20 -0700

    Add: surrounded-regions - submission-2

---
+++ Data Structures & Algorithms/surrounded-regions/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        n = len(board)
+        m = len(board[0])
+
+        for r in range(n):
+            for c in range(m):
+                if board[r][c] == 'O':
+                    if r == 0 or c == 0 or r == n-1 or c == m-1: 
+                        continue
+                    board[r][c] = 'X'
+
+        
