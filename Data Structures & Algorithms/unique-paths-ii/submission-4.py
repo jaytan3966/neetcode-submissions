b4e06c4f31a40266da93616c1d30e2a2e857d857
Jayden Tan <jaytan3966@gmail.com>
2026-08-12 14:34:29 -0700

    Add: unique-paths-ii - submission-4

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        n,m = len(obstacleGrid), len(obstacleGrid[0])
+        mem = {}
+        ans = 0
+
+        def dfs(r,c):
+            nonlocal ans
+
+            if not (0<=r<n and 0<=c<m) or obstacleGrid[r][c]: 
+                return
+            if r == n-1 and c == m-1:
+                ans+=1
+                return
+
+            dfs(r+1, c)
+            dfs(r, c+1)
+
+            return
+        dfs(0,0)
+        return ans
+            
+
+
+
+
