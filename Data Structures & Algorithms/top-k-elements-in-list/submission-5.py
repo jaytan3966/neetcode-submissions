9d23ee2dcbba7c7c2757570b7a8e825491d40cf0
Jayden Tan <jaytan3966@gmail.com>
2026-08-14 12:56:04 -0700

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        buckets = [[]]*len(nums)
+        counts = Counter(nums)
+
+        for num in counts:
+            buckets[counts[num]-1].append(num)
+            print(buckets)
+            print(counts[num])
+        
+        ans = []
+        for i in range(len(buckets)-1, -1, -1):
+            if len(ans) == k:
+                return ans
+            curBucket = buckets[i]
+            j = len(curBucket)-1
+            while j>-1 and len(ans) != k:
+                ans.append(curBucket[j])
+                j-=1
+        return ans
