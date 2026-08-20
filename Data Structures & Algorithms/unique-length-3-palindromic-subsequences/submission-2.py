017088c3ed3c9f8839a84262bd87db58845630fe
Jayden Tan <jaytan3966@gmail.com>
2026-08-20 12:29:11 -0700

    Add: unique-length-3-palindromic-subsequences - submission-2

---
+++ Data Structures & Algorithms/unique-length-3-palindromic-subsequences/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        ans = 0
+        
+        for ch in set(s):
+            left = s.find(ch)
+            right = s.rfind(ch)
+        
+            if right-left>1:
+                ans+=len(set(s[left+1:right]))
+        return ans
