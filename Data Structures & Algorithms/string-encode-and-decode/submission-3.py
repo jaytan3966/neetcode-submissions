0a87ca823de242756359d05c16c83f00ef79b2a9
Jayden Tan <jaytan3966@gmail.com>
2026-08-27 19:34:51 -0700

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if len(strs) == 1: return strs.join()
+        ans = ""
+        for word in strs:
+            ans+=word+" "
+        return ans
+    def decode(self, s: str) -> List[str]:
+        if len(s) == 1: return [s]
+        lst = s.split()
+        return lst
