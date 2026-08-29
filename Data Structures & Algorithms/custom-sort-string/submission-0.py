c8e9b5573dea1750bafb23518fe7fcac3fe7b592
Jayden Tan <jaytan3966@gmail.com>
2026-08-28 18:58:59 -0700

    Add: custom-sort-string - submission-0

---
+++ Data Structures & Algorithms/custom-sort-string/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        counts = Counter(s)
+        l = 0
+        n = len(order)
+        ans = ''
+
+        while l<n:
+            while counts.get(order[l],0)>0:
+                ans+=(order[l]*counts[order[l]])
+                counts[order[l]] = 0
+            l+=1
+        for c in counts:
+            count = counts[c]
+            ans+=(c*count)
+            counts[c] = 0
+        return ans
