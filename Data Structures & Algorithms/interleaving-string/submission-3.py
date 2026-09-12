247a57c9415784122d477823991ed0eb9f168e03
Jayden Tan <jaytan3966@gmail.com>
2026-09-12 12:16:38 -0700

    Add: interleaving-string - submission-3

---
+++ Data Structures & Algorithms/interleaving-string/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        n, m = len(s1), len(s2)
+        e = len(s3)
+        if n+m != e: return False
+
+        i, j, k = 0, 0, 0
+        memo = {}
+
+        def dfs(i, j, k):
+            if k==e:
+                return True
+
+            if (i, j, k) in memo:
+                return memo[(i, j, k)]
+            
+            memo[(i, j, k)] = False
+
+            if i<n and s1[i] == s3[k]:
+                memo[(i, j, k)] = memo[(i, j, k)] or dfs(i+1, j, k+1)
+            if j<m and s2[j] == s3[k]:
+                memo[(i, j, k)] = memo[(i, j, k)] or dfs(i, j+1, k+1)
+            
+            return memo[(i, j, k)]
+        return dfs(0,0,0)
