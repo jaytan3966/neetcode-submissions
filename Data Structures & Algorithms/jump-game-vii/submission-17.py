7ea279d14e9f16c108d1aae0fee9ef68ceed37d4
Jayden Tan <jaytan3966@gmail.com>
2026-09-23 11:34:57 -0700

    Add: jump-game-vii - submission-17

---
+++ Data Structures & Algorithms/jump-game-vii/submission-17.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        q = deque([0])
+        seen = set()
+        seen.add(0)
+        n = len(s)
+
+        while q:
+            ind = q.popleft()
+
+            if ind == n-1:
+                return True
+
+            start = ind+minJump
+            for i in range(start, min(ind+maxJump+1, n)):
+                if i not in seen and s[i] == '0':
+                    q.append(i)
+                    seen.add(i)
+        return False
