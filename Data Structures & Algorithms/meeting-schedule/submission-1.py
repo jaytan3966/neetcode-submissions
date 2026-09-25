77a44fb88d49fcfa85f46f07b2c702833ee88350
Jayden Tan <jaytan3966@gmail.com>
2026-09-24 18:16:03 -0700

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.py
@@ -0,0 +1,20 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        last = None
+
+        intervals.sort(key=lambda start: start.start)
+
+        for interval in intervals:
+            start, end = interval.start, interval.end
+            if last and (last[0]<=start<=last[1] or last[0]<=end<=last[1]):
+                return False
+            last = (start, end)
+        return True
