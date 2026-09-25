65cc24e92669086211698c7b32469395d278cd24
Jayden Tan <jaytan3966@gmail.com>
2026-09-24 18:23:29 -0700

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.py
@@ -0,0 +1,24 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        greatest = max(interval.end for interval in intervals)
+
+        array = [0]*(greatest+1)
+
+        for interval in intervals:
+            array[interval.start]+=1
+            array[interval.end]-=1
+            
+        cur = 0
+        for i in range(greatest+1):
+            array[i]+=cur
+            cur = array[i]
+        
+        return max(array)
